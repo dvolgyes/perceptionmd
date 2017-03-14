d35ad227d0a0b88a4f088dbe5123f57947f6ce28
David Völgyes <david.volgyes@ieee.org>
2017-03-14 12:09:48 +0100

    supporting non-linux tools

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
+import platform
 
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
+
+system = platform.system()
+
+reqs = ['cachetools','pydicom','textx','cython','matplotlib','kivy','pycontracts','docutils','pygame']
+
+if system != 'Linux':
+    reqs = ['cachetools','pydicom','textx','cython','matplotlib','kivy','pycontracts','docutils','pygame',
+    'kivy.deps.angle','kivy.deps.gstreamer','kivy.deps.sdl2','kivy.deps.glew']
 
 setup(
   include_data_files = True,
@@ -16,23 +25,12 @@ setup(
   packages = ['perceptionmd','perceptionmd/CTTools'], 
   scripts=['PerceptionMD.py',],
   data_files = [('perceptionmd', ['LICENSE.txt','perceptionmd/perception.tx']), 
-		('perceptionmd/widgets', ['perceptionmd/widgets/infoscreen.kv']), 
-		('perceptionmd/unittests/',['perceptionmd/unittests/travis-example.md'])],
+                ('perceptionmd/widgets', ['perceptionmd/widgets/infoscreen.kv']), 
+                ('perceptionmd/unittests/',['perceptionmd/unittests/travis-example.md'])],
   keywords = ['testing', 'example'],
   classifiers = [],
   license = 'AGPL3',
-  install_requires=[
-  "cachetools",
-  "pydicom",
-  "textx",
-  "cython",
-  "matplotlib",
-  "kivy",
-  "pycontracts",
-  "docutils",
-  "pygame",
-  ],
+  install_requires=reqs,
+#  download_url = 'https://github.com/dvolgyes/perceptionmd/archive/latest.tar.gz',
 )
 
-#  package_dir = {'perceptionmd': 'perceptionmd'},
-#  download_url = 'https://github.com/dvolgyes/perceptionmd/archive/0.1.2.tar.gz',
