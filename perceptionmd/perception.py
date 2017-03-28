9074be0b797c7356a98379a4a1fbf87df0bf08f4
David Volgyes <david.volgyes@ieee.org>
2017-03-28 13:50:31 +0200

    removing unicode literals for win64 compatibility

--- perceptionmd/perception.py
+++ perceptionmd/perception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-from __future__ import print_function, division, absolute_import, unicode_literals
+from __future__ import print_function, division, absolute_import
 
 import sys
 import os
