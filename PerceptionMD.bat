06ec8fa10f56f679b2705a672be532fb4be009cf
David Volgyes <david.volgyes@ieee.org>
2017-03-30 15:17:35 +0200

    Add files via upload

--- PerceptionMD.bat
+++ PerceptionMD.bat
@@ -1,1 +1,5 @@
-CALL %~dp0\..\python.exe %~dp0\PerceptionMD.py "%1"
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+IF /I "%argC%" EQU "0" CALL %~dp0\..\python.exe %~dp0\PerceptionMD.py example
+IF /I "%argC%" GEQ "1" CALL %~dp0\..\python.exe %~dp0\PerceptionMD.py "%1"
