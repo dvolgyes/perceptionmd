42c825b24aafbc1650e264870fb97fe69ee1d6ea
David Völgyes <david.volgyes@ieee.org>
2017-04-19 17:02:05 +0200

    [add] color map test and user defined color map example

--- perceptionmd/volumes/colors.py
+++ perceptionmd/volumes/colors.py
@@ -12,8 +12,4 @@ def create_colormap(name, cmap):
         array = np.clip(np.fromfile(
             cmap, sep=" ").reshape(-1, 3), 0, 255) / 255.0
         return LinearSegmentedColormap.from_list(name, array.tolist(), array.shape[0])
-    try:
-        return plt.cm.get_cmap(cmap)
-    except:
-        pass
-    return plt.cm.gray
+    return plt.cm.get_cmap(cmap)
