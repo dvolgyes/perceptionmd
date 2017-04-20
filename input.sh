b25bae9773eb90a17f1edf8a3224d26e9908c3c4
David Völgyes <david.volgyes@ieee.org>
2017-04-20 10:19:13 +0200

    [add] baselayer and alpha test

--- input.sh
+++ input.sh
@@ -20,38 +20,100 @@ for i in `seq 120`; do
   xdotool type Enter
   xdotool mousemove 300 300 click 1
   xdotool click --clearmodifiers 5
-  sleep 0.2
+  sleep 0.1
   xdotool click --clearmodifiers 4
-  sleep 0.2
+  sleep 0.1
 
   xdotool mousedown 1
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  50 0
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  -50 0
-  sleep 0.2
+  sleep 0.1
   xdotool mouseup 1
-  sleep 0.2
+  sleep 0.1
   xdotool mousedown 1
   xdotool mousemove_relative  0 50
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  0 -50
-  sleep 0.2
+  sleep 0.1
   xdotool mouseup 1
 
   xdotool mousedown 2
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  50 0
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  -50 0
-  sleep 0.2
+  sleep 0.1
   xdotool mouseup 2
-  sleep 0.2
+  sleep 0.1
   xdotool mousedown 2
   xdotool mousemove_relative  0 50
-  sleep 0.2
+  sleep 0.1
   xdotool mousemove_relative  0 -50
-  sleep 0.2
+  sleep 0.1
   xdotool mouseup 2
+
+  xdotool mousedown 3
+  sleep 0.1
+  xdotool mousemove_relative  50 0
+  sleep 0.1
+  xdotool mousemove_relative  -50 0
+  sleep 0.1
+  xdotool mouseup 3
+  sleep 0.1
+  xdotool mousedown 3
+  xdotool mousemove_relative  0 50
+  sleep 0.1
+  xdotool mousemove_relative  0 -50
+  sleep 0.1
+  xdotool mouseup 3
+
+  xdotool keydown ctrl
+  xdotool mousedown 1
+  sleep 0.1
+  xdotool mousemove_relative  50 0
+  sleep 0.1
+  xdotool mousemove_relative  -50 0
+  sleep 0.1
+  xdotool mouseup 1
+  sleep 0.1
+  xdotool mousedown 1
+  xdotool mousemove_relative  0 50
+  sleep 0.1
+  xdotool mousemove_relative  0 -50
+  sleep 0.1
+  xdotool mouseup 1
+
+  xdotool mousedown 2
+  sleep 0.1
+  xdotool mousemove_relative  50 0
+  sleep 0.1
+  xdotool mousemove_relative  -50 0
+  sleep 0.1
+  xdotool mouseup 2
+  sleep 0.1
+  xdotool mousedown 2
+  xdotool mousemove_relative  0 50
+  sleep 0.1
+  xdotool mousemove_relative  0 -50
+  sleep 0.1
+  xdotool mouseup 2
+
+  xdotool mousedown 3
+  sleep 0.1
+  xdotool mousemove_relative  50 0
+  sleep 0.1
+  xdotool mousemove_relative  -50 0
+  sleep 0.1
+  xdotool mouseup 3
+  sleep 0.1
+  xdotool mousedown 3
+  xdotool mousemove_relative  0 50
+  sleep 0.1
+  xdotool mousemove_relative  0 -50
+  sleep 0.1
+  xdotool mouseup 3
+  xdotool keyup ctrl
 
 done
