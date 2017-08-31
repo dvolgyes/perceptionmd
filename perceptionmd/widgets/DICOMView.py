869b648a31ee773fb623f4d4d51f14b4aef44924
David Völgyes <david.volgyes@ieee.org>
2017-08-31 16:42:10 +0200

    typo fix

--- perceptionmd/widgets/DICOMView.py
+++ perceptionmd/widgets/DICOMView.py
@@ -140,7 +140,7 @@ class DICOMView(BoxLayout):
         self.initialized = True
         if show:
             shift = self.wcenter - self.wwidth / 2.0
-            print(self.z_pos, shift, self.wwdith)
+            print(self.z_pos, shift, self.wwidth)
             array = np.clip((self.volume[self.z_pos, ...] - shift) / (self.wwidth / 255.0), 0, 255).astype(np.uint8)
             if array.shape[0] != array.shape[1]:
                 array = utils.padding_square(np.clip(array, 0, 255).astype(np.uint8))
